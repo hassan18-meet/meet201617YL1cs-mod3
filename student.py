715a1b810c602bb60c6b4de3fca804e53268b1d1
none <none>
2016-12-12 19:31:40 +0200

    made a class that describes a student

---
+++ student.py
@@ -0,0 +1,16 @@
+class student:
+    def __init__(self,name, hometown, age, height, favorite_icecream):
+
+        self.name=name
+        self.hometown=hometown
+        self.age=age
+        self.height=height
+        self.favorite_icecream=favorite_icecream
+
+    
+    def print_summary (self):
+        print (' my name is '+self.name, ' I live in '+self.hometown,' I am ' +self.age ' years old ', ' my hight is '+self.height ' meters ', ' my favorite icecream is ' + self.favorite_icecream)
+
+    def get_giraffe_gap()
+       return(500-self.height)
+    
