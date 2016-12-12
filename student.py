2ad542e0f433aac7ddfc3ad0e44debfa75a5a129
none <none>
2016-12-12 19:56:15 +0200

    completed student full sentence

--- student.py
+++ student.py
@@ -1,4 +1,4 @@
-class student:
+class Student:
     def __init__(self,name, hometown, age, height, favorite_icecream):
 
         self.name=name
@@ -9,8 +9,8 @@ class student:
 
     
     def print_summary (self):
-        print (' my name is '+self.name, ' I live in '+self.hometown,' I am ' +self.age ' years old ', ' my hight is '+self.height ' meters ', ' my favorite icecream is ' + self.favorite_icecream)
+        print (' my name is '+self.name + ' I live in '+self.hometown +' I am ' +str(self.age) + ' years old '+' my hight is '+str(self.height) +' meters '+ ' my favorite icecream is ' + self.favorite_icecream)
 
-    def get_giraffe_gap()
-       return(500-self.height)
+    #def get_giraffe_gap()
+       #return(500-self.height)
     
---
+++ test_student.py
@@ -0,0 +1,3 @@
+from student import Student
+hassan=Student ('Hassan', 'jerusalem', 16, 189 ,'vanilla')
+hassan.print_summary ()
